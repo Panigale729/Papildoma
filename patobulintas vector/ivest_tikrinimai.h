50b771f39607ae5536c828364ce501cc6807c3e6
Rokas Bandza <118292852+Panigale729@users.noreply.github.com>
2023-05-30 15:46:15 +0300

    Delete patobulintas vector directory

--- patobulintas vector/ivest_tikrinimai.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "mylib.h"
-
-int tikrinti_pazymi();                  //tikriname, ar ávestas paþymys yra nuo 0 iki 10
-string tikrinti_vard(int v);            //tikriname, ar vardas/pavardë sudaryta/-as tik ið raidþiø
-bool tikrinti_bool();                   //tikriname, ar ávedë bool reikðmæ
